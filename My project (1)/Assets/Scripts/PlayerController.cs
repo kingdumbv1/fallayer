348c39d439529dedf532607dc345d328d8434e24
kingdumbv1 <derpofth3dope@gmail.com>
2024-09-10 10:20:10 -0700

    camera fps and clamping
    yahhh camera works

--- My project (1)/Assets/Scripts/PlayerController.cs
+++ My project (1)/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody myRB; 
+    Rigidbody myRB;
+    Camera playercam;
+    Vector2 camRotation;
 
     public float speed = 10f;
+    public float jumpHeight = 5f;
+    public float mousesensitivity = 2.0f;
+    public float mousesensx = 2.0f;
+    public float mousesensy = 2.0f;
+
+    public float camlimit = 90f;
     // Start is called before the first frame update
     void Start()
     {
+        camRotation = Vector2.zero;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+
         myRB = GetComponent<Rigidbody>();
+        playercam = transform.GetChild(0).GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-        {
+        Vector3 temp = myRB.velocity;
 
-            myRB.AddForce(transform.forward * speed);
-        }
+        Quaternion mouseLook = playercam.transform.rotation;
+
+        Quaternion verticalRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+
+
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mousesensx;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mousesensy;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -90, 90);
+        playercam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
+        temp.z = Input.GetAxisRaw("Horizontal") * speed;
+        temp.x = Input.GetAxisRaw("Vertical") * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            temp.y = jumpHeight;
+
+        myRB.velocity = (transform.forward * temp.x) + (transform.right * temp.z) + (transform.up * temp.y);
     }
 }
