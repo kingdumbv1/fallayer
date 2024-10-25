d7ec5e945aae1ed485aa657463eefb6fc4c446a1
kingdumbv1 <derpofth3dope@gmail.com>
2024-10-25 09:49:23 -0700

    update 1

--- My project (1)/Assets/Scripts/FloorRandomizer.cs
+++ My project (1)/Assets/Scripts/FloorRandomizer.cs
@@ -6,8 +6,18 @@ public class FloorRandomizer : MonoBehaviour
 {
     public List<GameObject> tiles;
     public Material[] materials;
+    public Material[] gem;
+    public GameObject indicator;
 
+    public List<GameObject> red;
+    public List<GameObject> yellow;
+    public List<GameObject> blue;
+    public List<GameObject> green;
+    public List<GameObject> purple;
+
+    public int safe;
     public float timer = 0.5f;
+    public float deathInterval = 5f;
     public float switchInterval;
 
     private void Start()
@@ -31,10 +41,46 @@ public class FloorRandomizer : MonoBehaviour
             }
             foreach (GameObject item in tiles)
             {
-                item.GetComponent<MeshRenderer>().material = materials[Random.Range(0, materials.Length)];
+                int integer = Random.Range(0, materials.Length);
+                item.GetComponent<MeshRenderer>().material = materials[integer];
+                switch (integer)
+                {
+                    case 0:
+                        red.Add(item);
+                        break;
+                    case 1:
+                        yellow.Add(item);
+                        break;
+                    case 2:
+                        blue.Add(item);
+                        break;
+                    case 3:
+                        green.Add(item);
+                        break;
+                    case 4:
+                        purple.Add(item);
+                        break;
+                    default:
+                        break;
+                }
             }
+            safe = Random.Range(0, materials.Length);
+            indicator.GetComponent<MeshRenderer>().material = gem[safe];
+            yield return new WaitForSeconds(deathInterval);
+            if (safe != 0) red.ForEach((g) => g.SetActive(false));
+            if (safe != 1) yellow.ForEach((g) => g.SetActive(false));
+            if (safe != 2) blue.ForEach((g) => g.SetActive(false));
+            if (safe != 3) green.ForEach((g) => g.SetActive(false));
+            if (safe != 4) purple.ForEach((g) => g.SetActive(false));
             yield return new WaitForSeconds(switchInterval);
+            deathInterval *= 0.95f;
+            tiles.ForEach((g) => g.SetActive(true));
             timer = 0.5f;
+            red.Clear();
+            yellow.Clear();
+            blue.Clear();
+            green.Clear();
+            purple.Clear();
         }
     }
 }
